5b0e9e31de4721a624b8237753d91802b1961f60
Jair Moreno <jairmorenolml@gmail.com>
2024-09-23 17:48:10 -0500

    Probando Rama Creada

--- src/IU_DIGITAL/Main.java
+++ src/IU_DIGITAL/Main.java
@@ -2,7 +2,15 @@ package IU_DIGITAL;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            System.out.println("Hello World");
 
+            int[] numeros = {1, 2, 3};
+
+            System.out.println("Accediendo al índice 5: " + numeros[5]);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
 
     }
 }
