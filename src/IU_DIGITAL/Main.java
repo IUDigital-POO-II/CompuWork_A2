644e8fcead4028e4009e173b053c760e9e1b8828
Jair Moreno <jairmorenolml@gmail.com>
2024-09-24 15:25:05 -0500

    Creacion de departamentos, empleado permanentes y empleados temporales

--- src/IU_DIGITAL/Main.java
+++ src/IU_DIGITAL/Main.java
@@ -3,11 +3,16 @@ package IU_DIGITAL;
 public class Main {
     public static void main(String[] args) {
         try {
-            System.out.println("Hello World");
+            // Creación de departamentos
+            Departamento departamentoTI = new Departamento("Tecnología de la Información");
+            Departamento departamentoRH = new Departamento("Recursos Humanos");
 
-            int[] numeros = {1, 2, 3};
+            // Creación de empleados permanentes
+            EmpleadoPermanente empleado1 = new EmpleadoPermanente("Juan Pérez", "E001", "Desarrollador Senior", 5000);
+            EmpleadoPermanente empleado2 = new EmpleadoPermanente("Ana Gómez", "E002", "Gerente de TI", 8000);
 
-            System.out.println("Accediendo al índice 5: " + numeros[5]);
+            // Creación de empleados temporales
+            EmpleadoTemporal empleado3 = new EmpleadoTemporal("Carlos Ruiz", "E003", "Desarrollador Junior", 12);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
