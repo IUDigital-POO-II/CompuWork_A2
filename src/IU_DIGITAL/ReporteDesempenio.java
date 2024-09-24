d3799429ff85019494f993474a3cbb7189009cd1
Daniel-999-est <daniel.asprilla@est.iudigital.edu.co>
2024-09-23 22:13:11 -0500

    Implementar constructor en ReporteDesempenio que lanza excepción si el empleado es nulo

--- src/IU_DIGITAL/ReporteDesempenio.java
+++ src/IU_DIGITAL/ReporteDesempenio.java
@@ -3,4 +3,12 @@ package IU_DIGITAL;
 public class ReporteDesempenio {
     private Empleado empleado;
     private String evaluacion;
+
+    // Constructor que lanza la excepción si el empleado es nulo
+    public ReporteDesempenio(Empleado empleado, String evaluacion) throws ReporteNoGeneradoException {
+        if (empleado == null) {
+            throw new ReporteNoGeneradoException("No se puede generar el reporte porque el empleado es inexistente.");
+        }
+        this.empleado = empleado;
+        this.evaluacion = evaluacion;
 }
