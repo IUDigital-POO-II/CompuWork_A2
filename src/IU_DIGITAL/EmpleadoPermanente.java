a9553529caf46f7a009e1b8175b105e851ba0830
Sebastian Arenas Ocampo <sebastian.arenas@est.iudigital.edu.co>
2024-09-23 22:05:25 -0500

    Creacion del metodo constructor de la clase EmpleadoPermanente.java, que llama al constructor de la clase padre Empleado.java

--- src/IU_DIGITAL/EmpleadoPermanente.java
+++ src/IU_DIGITAL/EmpleadoPermanente.java
@@ -4,4 +4,9 @@ public class EmpleadoPermanente {
     // Atributo privado salario de la clase EmpleadoPermanente
     private double salario;
 
+    // Metodo constructor de la clase EmpleadoPermanente que llama al constructor de la clase padre Empleado
+    public EmpleadoPermanente(String nombre, String id, String puesto, double salario) {
+        super(nombre, id, puesto);
+        this.salario = salario;
+    }
 }
