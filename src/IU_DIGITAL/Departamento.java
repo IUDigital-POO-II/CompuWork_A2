47be4bba8d435187601026d1af9a334a78b61417
panda389142 <cristian.piedrahita@.iudigita.edu.co>
2024-09-24 16:27:53 -0500

    creando metodo constructor

--- src/IU_DIGITAL/Departamento.java
+++ src/IU_DIGITAL/Departamento.java
@@ -1,7 +1,13 @@
 package IU_DIGITAL;
 
 public class Departamento {
-    //atributos clase departamentos
+    //atributos clase departamento
     private String nombre;
     private ArrayList<Empleado> empleados;
+
+    //metodo constructor
+    public Departamento(String nombre) {
+        this.nombre = nombre;
+        this.empleados = new ArrayList<>();
+    }
 }
