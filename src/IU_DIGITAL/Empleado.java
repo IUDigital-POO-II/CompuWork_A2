c240c2c78a246d8f45b490080f6e5dd6a3467f8e
Sebastian Arenas Ocampo <sebastian.arenas@est.iudigital.edu.co>
2024-09-23 21:38:38 -0500

    Creación de la clase Empleado.java con sus respectivos atributos privados

--- src/IU_DIGITAL/Empleado.java
+++ src/IU_DIGITAL/Empleado.java
@@ -1,4 +1,9 @@
 package IU_DIGITAL;
 
 public class Empleado {
+    // Creacion de los atributos de la clase Empleado con los atributos privados nombre, id, puesto, Departamento
+    private String nombre;
+    private String id;
+    private String puesto;
+    private Departamento departamento;
 }
