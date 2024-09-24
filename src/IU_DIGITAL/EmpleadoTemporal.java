6c7597784483468f8c95e67f982eafa9676faeb3
Sebastian Arenas Ocampo <sebastian.arenas@est.iudigital.edu.co>
2024-09-23 22:19:22 -0500

    Creacion del metodo constructor de la clase EmpleadoTemporal.java y que llama a la clase padre Empleado.java

--- src/IU_DIGITAL/EmpleadoTemporal.java
+++ src/IU_DIGITAL/EmpleadoTemporal.java
@@ -4,4 +4,10 @@ public class EmpleadoTemporal {
     // Atributo privado duracionContratoMeses de la clase EmpleadoTemporal
     private int duracionContratoMeses;
 
+    // Constructor de la clase EmpleadoTemporal que llama a la clase padre Empleado
+    public EmpleadoTemporal(String nombre, String id, String puesto, int duracionContratoMeses) {
+        super(nombre, id, puesto);
+        this.duracionContratoMeses = duracionContratoMeses;
+    }
+
 }
